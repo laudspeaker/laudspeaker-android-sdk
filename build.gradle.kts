cdcf4b36f34cbcfb9f976e76c8195dd460f0c8f4
Moustafa Eid <568368+MoustafaEid@users.noreply.github.com>
2024-06-06 22:13:20 -0700

    root

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,52 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "com.laudspeaker",
+        artifactId = "laudspeaker-android",
+        version = "1.0.0"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("Laudspeaker Android SDK")
+        description.set("Android SDK for the Laudspeaker Customer Engagement Platform")
+        inceptionYear.set("2024")
+
+        url.set("https://github.com/laudspeaker/laudspeaker-android-sdk")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("mcharawi")
+                name.set("Mahamad Charawi")
+                email.set("mahamad@laudspeaker.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/laudspeaker/laudspeaker-android-sdk")
+        }
+
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
+}
+
 buildscript {
     repositories {
         mavenCentral()
