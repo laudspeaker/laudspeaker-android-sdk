244376a25b475271bd12ccb27e3c08caf6c8fbe4
Moustafa Eid <568368+MoustafaEid@users.noreply.github.com>
2024-06-06 22:09:49 -0700

    root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0-Beta5" apply false
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 buildscript {
