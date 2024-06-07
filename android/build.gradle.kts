bf9057c45a24b2fdb7f2ba07138edaa58815341d
Moustafa Eid <568368+MoustafaEid@users.noreply.github.com>
2024-06-06 21:10:31 -0700

    updated build.gradle files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 android {
@@ -28,8 +29,54 @@ android {
     }
 }
 
-dependencies {
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "com.laudspeaker",
+        artifactId = "laudspeaker-android",
+        version = "1.0.0"
+    )
 
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("Laudspeaker Android SDK")
+        description.set("Android SDK for the Laudspeaker Customer Engagement Platform")
+        inceptionYear.set("2024")
+
+        url.set("https://github.com/laudspeaker/laudspeaker-android-sdk")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("mcharawi")
+                name.set("Mahamad Charawi")
+                email.set("mahamad@laudspeaker.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/laudspeaker/laudspeaker-android-sdk")
+        }
+
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
+}
+
+dependencies {
+    implementation("com.jakewharton.threetenabp:threetenabp:1.3.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("io.socket:socket.io-client:2.1.0")
@@ -39,5 +86,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.code.gson:gson:2.10") // Use the latest version available
-    classpath 'com.android.tools.build:gradle:8.7.0'
+    implementation("io.sentry:sentry-android:7.8.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,25 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-
+    id("com.android.application") version "8.3.1" apply false
+    id("com.android.library") version "8.3.1" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0-Beta5" apply false
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0-Beta5")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
