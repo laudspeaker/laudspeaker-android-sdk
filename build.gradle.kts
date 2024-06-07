a1c321505ec51ea362a3090037f62d13317fef71
Moustafa Eid <568368+MoustafaEid@users.noreply.github.com>
2024-06-06 19:37:32 -0700

    build gradle change

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,5 +28,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.code.gson:gson:2.10") // Use the latest version available
-    classpath 'com.android.tools.build:gradle:2.3.0'
+    classpath 'com.android.tools.build:gradle:8.7.0'
 }
