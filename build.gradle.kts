8cf24f09427a2205c2b193a3174a961063275e00
Moustafa Eid <568368+MoustafaEid@users.noreply.github.com>
2024-06-06 21:18:00 -0700

    cleanup root gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.1" apply false
-    id("com.android.library") version "8.3.1" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0-Beta5" apply false
 }
 
 buildscript {
     repositories {
-        google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.1")
+        classpath("com.android.tools.build:gradle:8.8.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0-Beta5")
     }
 }
 
 allprojects {
     repositories {
-        google()
         mavenCentral()
     }
 }
